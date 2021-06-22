a147a8ec0eaf7230cfc1ba5b05e17ba9ea1187e3
peiyanni <nini9019260909@163.com>
2021-06-22 14:08:08 +0800

    BusinessConstant

---
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/constant/BusinessConstant.java
@@ -0,0 +1,14 @@
+package com.anjiplus.template.gaea.business.constant;
+
+/**
+ * 常量
+ * @author lr
+ * @since 2021-03-26
+ */
+public interface BusinessConstant {
+
+    /**
+     * 字典项重复
+     */
+    String DICT_ITEM_EXIST_GROUP = "dictItemExist";
+}
