bac71beec3eb5a2a1c6a7a7bd4690045b9669395
Raod <1130305001@qq.com>
2021-06-23 11:02:48 +0800

    ehcache

--- report-core/src/main/java/com/anjiplus/template/gaea/business/config/BusinessAutoConfiguration.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/config/BusinessAutoConfiguration.java
@@ -1,8 +1,11 @@
 package com.anjiplus.template.gaea.business.config;
 
+import com.anji.plus.gaea.cache.CacheHelper;
+import com.anjiplus.template.gaea.business.cache.ReportCacheHelper;
 import com.anjiplus.template.gaea.business.runner.ApplicationInitRunner;
-
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.cache.ehcache.EhCacheCache;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +28,24 @@ public class BusinessAutoConfiguration {
     @Bean
     public ApplicationInitRunner applicationInitRunner() {
         return new ApplicationInitRunner();
+    }
+
+    @Bean("gaeaCacheHelper")
+    public CacheHelper gaeaCacheHelper(){
+        return new ReportCacheHelper();
+    }
+
+    @Bean
+    public EhCacheCache ehCacheCache() {
+        return (EhCacheCache) ehCacheCacheManager().getCache("reportCache");
+    }
+
+    /**
+     * 创建ehCacheCacheManager
+     */
+    @Bean
+    public EhCacheCacheManager ehCacheCacheManager() {
+
+        return new EhCacheCacheManager();
     }
 }
