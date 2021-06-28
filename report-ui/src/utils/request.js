bec50fadaaa84f7b5abdedf0923595ecd11b5537
yanzili <978387851@qq.com>
2021-06-28 16:55:10 +0800

    登录 token校验

--- report-ui/src/utils/request.js
+++ report-ui/src/utils/request.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { Message, MessageBox } from 'element-ui'
 import store from '../store'
 import { getToken } from '@/utils/auth'
-
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api 的 base_url
@@ -17,7 +16,6 @@ service.interceptors.request.use(
   },
   error => {
     // Do something with request error
-    console.log(error) // for debug
     Promise.reject(error)
   }
 )
@@ -29,36 +27,37 @@ service.interceptors.response.use(
      * code为非20000是抛错 可结合自己业务进行修改
      */
     const res = response.data
-    if (res.code !== '200') {
+    // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
+    if (res.code == '50008' || res.code == '50012' || res.code == '50014') {
+      MessageBox.confirm(
+        '你已被登出，可以取消继续留在该页面，或者重新登录',
+        '重新登录',
+        {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).then(() => {
+        // store.dispatch('FedLogOut').then(() => {
+        //   // location.reload() // 为了重新实例化vue-router对象 避免bug
+        //   window.location.href = "/";
+        // })
+        location.reload();
+        window.location.href = "/";
+      })
+    }
+    else if (res.code !== '200') {
       Message({
         message: res.message,
         type: 'error',
         duration: 5 * 1000
       })
-
-      // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        MessageBox.confirm(
-          '你已被登出，可以取消继续留在该页面，或者重新登录',
-          '确定登出',
-          {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '取消',
-            type: 'warning'
-          }
-        ).then(() => {
-          store.dispatch('FedLogOut').then(() => {
-            location.reload() // 为了重新实例化vue-router对象 避免bug
-          })
-        })
-      }
       return Promise.reject('error')
     } else {
       return response.data
     }
   },
   error => {
-    console.log('err' + error) // for debug
     Message({
       message: error.message,
       type: 'error',
