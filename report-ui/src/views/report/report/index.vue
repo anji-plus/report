564289a4ae045a937294e20a0046790cce9f00df
yanzili <978387851@qq.com>
2021-06-28 15:40:47 +0800

    样式修改

--- report-ui/src/views/report/report/index.vue
+++ report-ui/src/views/report/report/index.vue
@@ -9,7 +9,7 @@
     <el-form ref="form"
              :model="params"
              :rules="rules"
-             label-width="100px">
+             label-width="120px">
       <!-- 搜索 -->
       <el-row :gutter="10">
         <el-col :xs="24"
@@ -17,8 +17,7 @@
                 :md="12"
                 :lg="6"
                 :xl="4">
-          <el-form-item label="名称"
-                        label-width="120px">
+          <el-form-item label="名称">
             <el-input v-model="params.reportName"
                       size="mini"
                       clearable
@@ -31,8 +30,7 @@
                 :md="12"
                 :lg="6"
                 :xl="4">
-          <el-form-item label="报表编码"
-                        label-width="120px">
+          <el-form-item label="报表编码">
             <el-input v-model="params.reportCode"
                       size="mini"
                       clearable
@@ -40,19 +38,18 @@
                       class="filter-item" />
           </el-form-item>
         </el-col>
-        <el-col :xs="24"
+        <!-- <el-col :xs="24"
                 :sm="20"
                 :md="12"
                 :lg="6"
                 :xl="4">
-          <el-form-item label="报表类型"
-                        label-width="120px"
+          <el-form-item label="报表类型" 
                         size="mini">
-            <!-- <Dictionary v-model="params.reportType"
+            <Dictionary v-model="params.reportType"
                         :updata-dict="params.reportType"
-                        :dict-key="'REPORT_TYPE'" /> -->
+                        :dict-key="'REPORT_TYPE'" />
           </el-form-item>
-        </el-col>
+        </el-col> -->
         <el-col :xs="24"
                 :sm="20"
                 :md="4"
@@ -412,3 +409,13 @@ export default {
   },
 }
 </script>
+<style scoped>
+.el-form-item__label {
+  line-height: 30px !important;
+  height: 30px;
+}
+.el-form-item__content {
+  line-height: 30px !important;
+  height: 30px;
+}
+</style>
