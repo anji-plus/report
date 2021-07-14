57ea6700e45841e490c40fbd5ae2d6bb6b5ae3fb
qianming <qianming@anji-plus.com>
2021-07-14 16:05:46 +0800

    文本数据字典新增

---
+++ report-core/src/main/resources/db/migration/V1.0.3__update_table.sql
@@ -0,0 +1,4 @@
+/*
+字典更新
+*/
+INSERT INTO `aj_report`.`gaea_dict_item`(`dict_code`, `item_name`, `item_value`, `item_extend`, `enabled`, `locale`, `remark`, `sort`, `create_by`, `create_time`, `update_by`, `update_time`, `version`) VALUES ('CHART_PROPERTIES', '文本数字', 'text', NULL, 1, 'zh', NULL, 10, 'admin', now(), 'admin', now(), 1);
