8462b0e3a7d2b76f55a48bafca5616c190d0b291
木子李·De <lide1202@hotmail.com>
2021-07-17 23:02:09 +0800

    Create AccessUserMapper.java

---
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/modules/accessuser/dao/AccessUserMapper.java
@@ -0,0 +1,21 @@
+package com.anjiplus.template.gaea.business.modules.accessuser.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.anji.plus.gaea.curd.mapper.GaeaBaseMapper;
+import com.anjiplus.template.gaea.business.modules.accessuser.dao.entity.AccessUser;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+/**
+* AccessUser Mapper
+* @author 木子李·De <lide1202@hotmail.com>
+* @date 2019-02-17 08:50:11.902
+**/
+@Mapper
+public interface AccessUserMapper extends GaeaBaseMapper<AccessUser> {
+
+}
