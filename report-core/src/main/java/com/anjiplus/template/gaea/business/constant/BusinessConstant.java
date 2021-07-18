a11684436995586ae36fba92b6e5f4a608a9ce46
peiyanni <nini9019260909@163.com>
2021-07-18 14:30:48 +0800

    Update BusinessConstant.java

--- report-core/src/main/java/com/anjiplus/template/gaea/business/constant/BusinessConstant.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/constant/BusinessConstant.java
@@ -11,4 +11,19 @@ public interface BusinessConstant {
      * 字典项重复
      */
     String DICT_ITEM_EXIST_GROUP = "dictItemExist";
+
+    /**
+     * 所有url的权限码缓存key
+     */
+    String GAEA_SECURITY_AUTHORITIES= "gaea:security:authorities:all";
+
+    /**
+     * 用户登录的token缓存key
+     */
+    String GAEA_SECURITY_LOGIN_TOKEN = "gaea:security:login:token:%s";
+
+    /**
+     * 用户登录的主信息缓存信息
+     */
+    String GAEA_SECURITY_LOGIN_USER = "gaea:security:login:user:%s";
 }
