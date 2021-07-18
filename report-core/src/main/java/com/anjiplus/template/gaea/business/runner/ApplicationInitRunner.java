78373309947558132321dbb8d9ed182aeb4ad880
木子李·De <lide1202@hotmail.com>
2021-07-18 14:33:55 +0800

    update for authrity scan

--- report-core/src/main/java/com/anjiplus/template/gaea/business/runner/ApplicationInitRunner.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/runner/ApplicationInitRunner.java
@@ -1,26 +1,23 @@
 package com.anjiplus.template.gaea.business.runner;
 
-import com.anji.plus.gaea.constant.Enabled;
-import com.anji.plus.gaea.init.InitRequestUrlMappings;
+import com.anjiplus.template.gaea.business.modules.accessauthority.service.AccessAuthorityService;
 import com.anjiplus.template.gaea.business.modules.dict.service.GaeaDictService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
-import java.util.List;
-
 /**
- * 启动加载后执行
- * @author lr
- * @since 2021-04-08
- */
+ * @desc 启动加载器
+ * @author WongBin <abc13579d@163.com>
+ * @date 2019-02-17 08:50:10.009
+ **/
 public class ApplicationInitRunner implements ApplicationRunner {
 
     @Autowired
     private GaeaDictService gaeaDictService;
 
     @Autowired
-    private InitRequestUrlMappings initRequestUrlMappings;
+    private AccessAuthorityService accessAuthorityService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -28,9 +25,7 @@ public class ApplicationInitRunner implements ApplicationRunner {
         //1、数据字典刷新
         // gaeaDictService.refreshCache(null);
 
-        //获取当前应用中所有的请求信息
-        //List<InitRequestUrlMappings.RequestInfo> requestInfos = initRequestUrlMappings.getRequestInfos(Enabled.YES.getValue());
-
-        //System.out.println(requestInfos);
+        //2. 建立url权限拦截体系
+        accessAuthorityService.scanGaeaSecurityAuthorities();
     }
 }
