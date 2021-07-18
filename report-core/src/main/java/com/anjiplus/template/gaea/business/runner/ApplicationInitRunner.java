ed3a022c615e9e60151a27ddd17fce01f90d6d12
木子李·De <lide1202@hotmail.com>
2021-07-18 08:09:06 +0800

    Update ApplicationInitRunner.java

--- report-core/src/main/java/com/anjiplus/template/gaea/business/runner/ApplicationInitRunner.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/runner/ApplicationInitRunner.java
@@ -1,9 +1,13 @@
 package com.anjiplus.template.gaea.business.runner;
 
+import com.anji.plus.gaea.constant.Enabled;
+import com.anji.plus.gaea.init.InitRequestUrlMappings;
 import com.anjiplus.template.gaea.business.modules.dict.service.GaeaDictService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+
+import java.util.List;
 
 /**
  * 启动加载后执行
@@ -15,10 +19,18 @@ public class ApplicationInitRunner implements ApplicationRunner {
     @Autowired
     private GaeaDictService gaeaDictService;
 
+    @Autowired
+    private InitRequestUrlMappings initRequestUrlMappings;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         //1、数据字典刷新
-        gaeaDictService.refreshCache(null);
+        // gaeaDictService.refreshCache(null);
+
+        //获取当前应用中所有的请求信息
+        //List<InitRequestUrlMappings.RequestInfo> requestInfos = initRequestUrlMappings.getRequestInfos(Enabled.YES.getValue());
+
+        //System.out.println(requestInfos);
     }
 }
