e176f81a0fc987f6328554afeb6fe0f861279852
Raod <1130305001@qq.com>
2021-07-20 11:40:42 +0800

    start.bat

--- report-core/src/main/assembly/bin/start.bat
+++ report-core/src/main/assembly/bin/start.bat
@@ -7,6 +7,6 @@ cd ..\bin
 
 set JAVA_OPTS=" -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k "
 
-java -Xbootclasspath/a:%LIB_JARS% %JAVA_OPTS% -jar -Dspring.config.location=..\conf\bootstrap-dev.yml ..\lib\report-core-1.0.0-SNAPSHOT.jar
+java -jar -Dspring.config.location=..\conf\bootstrap-dev.yml ..\lib\report-core-1.0.0-SNAPSHOT.jar -Xbootclasspath/a:%LIB_JARS% %JAVA_OPTS%
 
 pause
