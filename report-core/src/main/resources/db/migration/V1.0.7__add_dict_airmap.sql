1e1f9aa1dc40fe3ce506d5a2499cf336ee44d22d
qianming <qianming@anji-plus.com>
2021-07-22 13:35:07 +0800

    新增气泡地图字典

---
+++ report-core/src/main/resources/db/migration/V1.0.7__add_dict_airmap.sql
@@ -0,0 +1,5 @@
+/*
+字典更新
+*/
+INSERT INTO `aj_report`.`gaea_dict_item`(`dict_code`, `item_name`, `item_value`, `item_extend`, `enabled`, `locale`, `remark`, `sort`, `create_by`, `create_time`, `update_by`, `update_time`, `version`) VALUES ('CHART_PROPERTIES', '地图地名', 'name', NULL, 1, 'zh', NULL, 10, 'admin', now(), 'admin', now(), 1);
+INSERT INTO `aj_report`.`gaea_dict_item`(`dict_code`, `item_name`, `item_value`, `item_extend`, `enabled`, `locale`, `remark`, `sort`, `create_by`, `create_time`, `update_by`, `update_time`, `version`) VALUES ('CHART_PROPERTIES', '地图数值', 'value', NULL, 1, 'zh', NULL, 10, 'admin', now(), 'admin', now(), 1);
