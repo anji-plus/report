25c68a4a650e7c387c5c4dec9ab92bef9936c793
木子李·De <lide1202@hotmail.com>
2021-07-24 11:11:50 +0800

    Update start.bat

--- report-core/src/main/assembly/bin/start.bat
+++ report-core/src/main/assembly/bin/start.bat
@@ -1,13 +1,18 @@
 @echo off & setlocal enabledelayedexpansion
 
 cd ..\lib
+
+for /f "delims=" %%i in ('dir /a-d /b /on aj-report*.jar') do (
+   set BOOT_JAR=%%i
+)
+
 for %%i in (*) do set LIB_JARS=!LIB_JARS!..\lib\%%i;
 cd ..\bin
 
 echo %LIB_JARS%
+echo ""
 
-set JAVA_OPTS=" -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k "
+set JAVA_HOME=D:\App\Java\jdk1.8.0_172
+set JAVA_OPTS= -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k
 
-java $JAVA_OPTS -jar -Dspring.config.location=..\conf\bootstrap.yml ..\lib\aj-report-*.jar -Xbootclasspath/a:%LIB_JARS%
-
-pause
+%JAVA_HOME%\bin\java %JAVA_OPTS% -jar -Dspring.config.location=..\conf\bootstrap.yml ..\lib\%BOOT_JAR% -Xbootclasspath/a:%LIB_JARS%
