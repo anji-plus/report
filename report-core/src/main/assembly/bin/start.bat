4e1eb66470db60a65ba2d65a927d2c7a1a256d43
木子李·De <lide1202@hotmail.com>
2021-07-24 12:19:35 +0800

    Update start.bat

--- report-core/src/main/assembly/bin/start.bat
+++ report-core/src/main/assembly/bin/start.bat
@@ -1,18 +1,42 @@
 @echo off & setlocal enabledelayedexpansion
 
-cd ..\lib
-
-for /f "delims=" %%i in ('dir /a-d /b /on aj-report*.jar') do (
-   set BOOT_JAR=%%i
+rem 判断cmd中是否有JAVA_HOME
+rem set JAVA_HOME=D:\App\Java\jdk1.8.0_172
+IF "%JAVA_HOME%" == "" (
+echo end
+	goto END
+) ELSE (
+echo start
+    goto START
 )
 
-for %%i in (*) do set LIB_JARS=!LIB_JARS!..\lib\%%i;
-cd ..\bin
 
-echo %LIB_JARS%
-echo ""
+:END
+	rem cmd中未找到JAVA_HOME，请在此启动文件中第4行指定，然后尝试
+    echo Not JAVA_HOME Find, Please add JAVA_HOME in this file line 4
+	pause
+	exit
 
-set JAVA_HOME=D:\App\Java\jdk1.8.0_172
-set JAVA_OPTS= -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=128m -Xss256k
+:START
+	cd ..\lib
 
-%JAVA_HOME%\bin\java %JAVA_OPTS% -jar -Dspring.config.location=..\conf\bootstrap.yml ..\lib\%BOOT_JAR% -Xbootclasspath/a:%LIB_JARS%
+	rem 查找aj-report*.jar启动文件，版本号每次会变
+	for /f "delims=" %%i in ('dir /a-d /b /on aj-report*.jar') do (
+	   set BOOT_JAR=%%i
+	)
+	echo %BOOT_JAR%
+
+	rem 查找附加的驱动，加到java启动的classpath中
+	for %%i in (*) do (
+		if "%%i" neq "%BOOT_JAR%" (
+			set LIB_JARS=!LIB_JARS!..\lib\%%i;
+		)
+	)
+	echo %LIB_JARS%
+
+	rem 启动内存设置，请根据自己需要调整
+	cd ..\bin
+	set JAVA_OPTS= -server -Xms1g -Xmx2g -Xmn256m -XX:PermSize=128m -Xss256k
+
+	rem 正式启动
+	%JAVA_HOME%\bin\java %JAVA_OPTS% -jar -Dspring.config.location=..\conf\bootstrap.yml ..\lib\%BOOT_JAR% -Xbootclasspath/a:%LIB_JARS%
