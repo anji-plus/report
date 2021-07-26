6ec0faf3e94b7a11905f8599c732804dbd36c2f1
qianlishi <1432731663@qq.com>
2021-07-26 09:26:46 +0800

    update iframe

--- report-ui/src/views/report/bigscreen/designer/widget/widgetIframe.vue
+++ report-ui/src/views/report/bigscreen/designer/widget/widgetIframe.vue
@@ -1,54 +1,60 @@
 <template>
-  <iframe :style="styleColor" :src="styleColor.iframeAdress" width="100%" height="100%" />
+  <iframe
+    :style="styleColor"
+    :src="styleColor.iframeAdress"
+    width="100%"
+    height="100%"
+  />
 </template>
 
 <script>
 export default {
-  name: 'WidgetIframe',
+  name: "WidgetIframe",
   components: {},
   props: {
     value: Object,
-    ispreview: Boolean,
+    ispreview: Boolean
   },
   data() {
     return {
-      options: {},
-    }
+      options: {}
+    };
   },
   computed: {
     transStyle() {
-      return this.objToOne(this.options)
+      return this.objToOne(this.options);
     },
     styleColor() {
       return {
-        position: this.ispreview ? 'absolute' : 'static',
-        width: this.transStyle.width + 'px',
-        height: this.transStyle.height + 'px',
-        left: this.transStyle.left + 'px',
-        top: this.transStyle.top + 'px',
-        right: this.transStyle.right + 'px',
-        iframeAdress: this.transStyle.iframeAdress,
-      }
-    },
+        position: this.ispreview ? "absolute" : "static",
+        width: this.transStyle.width + "px",
+        height: this.transStyle.height + "px",
+        left: this.transStyle.left + "px",
+        top: this.transStyle.top + "px",
+        right: this.transStyle.right + "px",
+        iframeAdress: this.transStyle.iframeAdress
+      };
+    }
   },
   watch: {
     value: {
       handler(val) {
-        this.options = val
+        this.options = val;
       },
-      deep: true,
-    },
+      deep: true
+    }
   },
   mounted() {
-    this.options = this.value
+    this.options = this.value;
   },
-  methods: {},
-}
+  methods: {}
+};
 </script>
 
 <style scoped lang="scss">
 iframe {
   width: 100%;
   height: 100%;
+  border: none;
 }
 </style>
