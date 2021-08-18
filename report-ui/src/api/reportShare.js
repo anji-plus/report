cce203363b1c71d4efc7e405370deb8f27348a5a
Raod <1130305001@qq.com>
2021-08-18 17:05:26 +0800

    报表分享

---
+++ report-ui/src/api/reportShare.js
@@ -0,0 +1,42 @@
+import request from '@/utils/request'
+
+export function reportShareList(params) {
+  return request({
+    url: 'reportShare/pageList',
+    method: 'GET',
+    params,
+  })
+}
+
+export function reportShareAdd(data) {
+  return request({
+    url: 'reportShare',
+    method: 'post',
+    data
+  })
+}
+
+export function reportShareDeleteBatch(data) {
+return request({
+url: 'reportShare/delete/batch',
+method: 'post',
+data
+})
+}
+
+export function reportShareUpdate(data) {
+  return request({
+    url: 'reportShare',
+    method: 'put', data,
+  })
+}
+
+export function reportShareDetail(data) {
+  return request({
+    url: 'reportShare/' + data.id,
+    method: 'get',
+    params: { accessKey: data.accessKey }
+  })
+}
+
+export default { reportShareList, reportShareAdd, reportShareDeleteBatch, reportShareUpdate, reportShareDetail }
