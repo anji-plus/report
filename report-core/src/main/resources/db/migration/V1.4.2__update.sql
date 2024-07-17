1127d312bc9694d969882326c88b793499b95d1f
Devli <5003188+lide1202@user.noreply.gitee.com>
2024-07-17 09:04:45 +0800

    增加postgresql数据源支持

---
+++ report-core/src/main/resources/db/migration/V1.4.2__update.sql
@@ -0,0 +1,3 @@
+Replace INTO gaea_dict_item
+(dict_code, item_name, item_value, item_extend, enabled, locale, remark, sort, create_by, create_time, update_by, update_time, version)
+VALUES('SOURCE_TYPE', 'postgresql', 'postgresql', '[{"label":"driverName","value":"org.postgresql.Driver","labelValue":"驱动类"},{"label":"jdbcUrl","value":"jdbc:postgresql://127.0.0.1:5432/pgdb","labelValue":"连接串"},{"label":"username","value":"root","labelValue":"用户名"},{"label":"password","value":"root","labelValue":"密码"}]', 1, 'zh', NULL, 2, 'admin', '2021-03-23 10:54:08', 'admin', '2021-03-23 10:54:08', 1);
