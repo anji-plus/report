1b79dfe6b5186ad1b65f6b0ec7c77def47dc612e
Devli <5003188+lide1202@user.noreply.gitee.com>
2024-08-26 09:21:11 +0800

    删除swagger相关注解

--- report-core/src/main/java/com/anjiplus/template/gaea/business/modules/file/entity/GaeaFile.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/modules/file/entity/GaeaFile.java
@@ -2,7 +2,6 @@ package com.anjiplus.template.gaea.business.modules.file.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.anji.plus.gaea.curd.entity.GaeaBaseEntity;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,18 +16,18 @@ import java.io.Serializable;
 @Data
 public class GaeaFile extends GaeaBaseEntity implements Serializable {
 
-    @ApiModelProperty(value = "文件标识")
+    /** 文件标识 */
     private String fileId;
 
-    @ApiModelProperty(value = "文件类型")
+    /** 文件类型 */
     private String fileType;
 
-    @ApiModelProperty(value = "文件路径")
+    /** 文件路径 */
     private String filePath;
 
-    @ApiModelProperty(value = "url路径")
+    /** url路径 */
     private String urlPath;
 
-    @ApiModelProperty(value = "内容说明")
+    /** 内容说明 */
     private String fileInstruction;
 }
