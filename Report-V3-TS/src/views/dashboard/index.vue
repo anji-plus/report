53329f7527852f9d8f2408c3aa95060013a108e9
qianlishi <1432731663@qq.com>
2024-12-13 15:52:34 +0800

    首页

--- Report-V3-TS/src/views/dashboard/index.vue
+++ Report-V3-TS/src/views/dashboard/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>111</div>
+  <div class="index-container">
+    <div class="bg"></div>
+  </div>
 </template>
-<script setup lang="ts">
+<script setup lang="ts"></script>
+<style lang="less" scoped>
+.index-container {
+  width: 100%;
+  height: calc(100vh - 118px);
+  overflow: hidden;
+  background: #fff;
+  background: -webkit-linear-gradient(75deg,
+      #e9f0fb,
+      #5bc4ef);
+  /* Safari 5.1 - 6.0 */
+  background: -o-linear-gradient(75deg,
+      #e9f0fb,
+      #5bc4ef);
+  /* Opera 11.1 - 12.0 */
+  background: -moz-linear-gradient(75deg,
+      #e9f0fb,
+      #5bc4ef);
+  /* Firefox 3.6 - 15 */
+  background: linear-gradient(75deg, #e9f0fb, #e9f0fb);
 
-</script>
-<style lang="less" scoped></style>
+  .bg {
+    width: 100%;
+    height: 100%;
+    background: url('@/assets/imgs/index-center.png') no-repeat;
+    background-size: cover;
+  }
+
+}
+</style>
