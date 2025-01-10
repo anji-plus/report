3a7fc24afffb4c3ae221113e2d3a1714905ec041
qianlishi <1432731663@qq.com>
2025-01-10 13:56:14 +0800

    角色管理

---
+++ Report-V3-TS/src/api/access/accessRole.ts
@@ -0,0 +1,55 @@
+import { http } from '@/utils/http/axios';
+
+export function getPageList (params) {
+  return http.request({
+    url: 'accessRole/pageList',
+    method: 'GET',
+    params,
+  })
+}
+
+export function toAddApi (data) {
+  return http.request({
+    url: 'accessRole',
+    method: 'post',
+    data
+  })
+}
+
+export function toDeleteApi (data) {
+  return http.request({
+    url: 'accessRole/delete/batch',
+    method: 'post',
+    data
+  })
+}
+
+export function toUpdateApi (data) {
+  return http.request({
+    url: 'accessRole',
+    method: 'put', data,
+  })
+}
+
+export function toGetDataDetailApi (data) {
+  return http.request({
+    url: 'accessRole/' + data.id,
+    method: 'get',
+    params: { accessKey: data.accessKey }
+  })
+}
+
+export function accessRoleAuthorityTree (roleCode) {
+  return http.request({
+    url: 'accessRole/authorityTree/' + roleCode,
+    method: 'get',
+  })
+}
+
+export function saveAuthorityTree (data) {
+  return http.request({
+    url: 'accessRole/saveAuthorityTree',
+    method: 'post',
+    data
+  })
+}
--- Report-V3-TS/src/views/access/accessRole/index.vue
+++ Report-V3-TS/src/views/access/accessRole/index.vue
@@ -1,8 +1,62 @@
 <template>
-  <div>角色管理
+  <div class="view-container">
+    <JsqCrud @register="register" />
   </div>
 </template>
 <script lang="ts" setup>
+  import { JsqCrud, useCrud } from '@/components/Base/Jsq-crud';
+  import { getFormSchemas, getTableButtons, getDialogRecordingSchemas, getTableColumns } from './utils/schemas';
+  import { toGetPageList, toAddApi, toDeleteApi, toUpdateApi, toGetDataDetailApi } from '@/api/access/accessAuthority'
 
+  // 新增
+  const addClick = () => {
+    toAdd()
+  }
+
+  // 批量删除
+  const removeAll = () => {
+    toRemoveAll()
+  }
+
+  // 编辑
+  const updateClick = (row) => {
+    toUpdate(row)
+  }
+
+  // 删除
+  const removeSingle = (row) => {
+    toRemove(row)
+  }
+
+  const { rowsButtons } = getTableButtons({ addClick, removeAll })
+  const { columns } = getTableColumns({ updateClick, removeSingle })
+
+  const [register, { toAdd, toUpdate, toRemoveAll, toRemove }] = useCrud({
+    searchFormOption: {
+      schemas: getFormSchemas({}).value,
+    },
+    tableButtonsOptions: {
+      // 添加naive 按钮其他属性
+      tableButtons: rowsButtons
+    },
+    dialogRecordingData: {
+      width: 800,
+      size: "small",
+      labelPlacement: "left",
+      labelWidth: 100,
+      schemas: getDialogRecordingSchemas()
+    },
+    tableOptions: {
+      // navie table配置
+      columns: columns, // 表格配置
+    },
+    apiOptions: {
+      queryApi: toGetPageList, // 查询
+      addApi: toAddApi, // 新增
+      removeApi: toDeleteApi, // 删除
+      updateApi: toUpdateApi, // 修改
+      getDataByIdApi: toGetDataDetailApi // 查询详情页
+    }
+  });
 </script>
 <style lang="less" scoped></style>
