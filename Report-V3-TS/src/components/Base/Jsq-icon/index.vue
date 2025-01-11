d7ff329439d3809c6cec7e7ad5ab10a7b4950297
qianlishi <1432731663@qq.com>
2025-01-11 22:08:26 +0800

    公共icon组件

---
+++ Report-V3-TS/src/components/Base/Jsq-icon/index.vue
@@ -0,0 +1,43 @@
+<!-- 
+  使用规则 <s-icon name="svgName" color='#000' :size="20" /> 
+-->
+<template>
+  <svg aria-hidden="true" class="icon" :style="computedStyle">
+    <use :xlink:href="`#${name}`" :fill="color" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: false,
+    },
+    color: {
+      type: String,
+      default: '',
+    },
+    size: {
+      type: Number,
+    },
+  });
+
+  const computedStyle = computed(() =>
+    props.size
+      ? { width: props.size + "px", height: props.size + "px", color: props.color }
+      : { color: props.color },
+  );
+</script>
+
+<style lang="less" scoped>
+  .icon {
+    display: flex;
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em; /* 因icon大小被设置为和字体大小一致，而span等标签的下边缘会和字体的基线对齐，故需设置一个往下的偏移比例，来纠正视觉上的未对齐效果 */
+    fill: currentColor; /* 定义元素的颜色，currentColor是一个变量，这个变量的值就表示当前元素的color值，如果当前元素未设置color值，则从父元素继承 */
+    overflow: hidden;
+  }
+</style>
